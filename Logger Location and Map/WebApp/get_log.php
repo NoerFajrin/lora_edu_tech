bb6f9a6deba8d14190eeed86a698ae3df4cf8a18
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-07 07:20:02 +0700

    Add files via upload

---
+++ Logger Location and Map/WebApp/get_log.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "monitoring";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi database gagal: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM mylog ORDER BY timestamp DESC LIMIT 10";
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+echo json_encode($data);
+?>
